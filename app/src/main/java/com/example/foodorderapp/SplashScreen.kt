a4cd188925e8efd5a2977c3cf0853ad65a462cfe
ns-develops <ns-develops@users.noreply.github.com>
2025-03-02 09:02:18 +0100

    animate letters instead, and loginactivity

--- app/src/main/java/com/example/foodorderapp/SplashScreen.kt
+++ app/src/main/java/com/example/foodorderapp/SplashScreen.kt
@@ -3,30 +3,47 @@ package com.example.foodorderapp
 import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
-import androidx.activity.enableEdgeToEdge
+import android.view.animation.AlphaAnimation
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class SplashScreen : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_splash_screen)
 
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+        val splashText: TextView = findViewById(R.id.splashText)
+        val splashText2: TextView = findViewById(R.id.splashText2)
 
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
 
-            return@setOnApplyWindowInsetsListener insets
-        }
+        animateText(splashText, 0, "Crepes and")
+        animateText(splashText2, 3000, "Co.")
 
-        // Waits 3 sek before MainActivity
+        // waits 6 sek before LoginActivity
         Handler().postDelayed({
-            val intent = Intent(this, MainActivity::class.java)
+            val intent = Intent(this, LoginActivity::class.java)
             startActivity(intent)
             finish()
-        }, 3000)
+        }, 8000)
+    }
+
+    // animate
+    private fun animateText(textView: TextView, delay: Long, text: String) {
+        val handler = Handler()
+        for (i in text.indices) {
+            handler.postDelayed({
+                val currentText = text.substring(0, i + 1)
+                textView.text = currentText
+
+
+                val fadeIn = AlphaAnimation(0f, 1f)
+                fadeIn.duration = 500 // duration time for letters
+                textView.startAnimation(fadeIn)
+            }, (i * 500).toLong() + delay) // time between letters
+        }
+
+
+        textView.alpha = 1f
     }
 }
